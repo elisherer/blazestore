1ace33bb629e173fc1d115ce1429ced506cf5d19
Eli Sherer <eli.sherer@gmail.com>
2021-07-21 00:42:24 +0300

    remove unneeded deps, update license to MIT

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import "./monaco/monaco.init";
 
 ReactDOM.render(
   <BrowserRouter>
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
